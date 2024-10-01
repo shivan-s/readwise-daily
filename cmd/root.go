7188c5d262492c0010e7f8c7e5040e1cc85d049d
Shivan Sivakumaran <me@shivan.xyz>
2024-10-01 23:29:05 +1300

    chore: wip

--- cmd/root.go
+++ cmd/root.go
@@ -1,11 +1,16 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/charmbracelet/log"
+	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -26,28 +31,64 @@ func obtainToken() string {
 	return token
 }
 
-func getHighlight() string {
+func getHighlight() readwiseHighlight {
 	const url = "https://readwise.io/api/v2/review/"
 	token := obtainToken()
 
 	client := http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
+		log.Error("Failed to establish the HTTP request")
+		log.Error(err.Error())
 		panic(err)
 	}
 	req.Header.Add("Authorization", fmt.Sprintf("Token %s", token))
 	res, err := client.Do(req)
 	if err != nil {
+		log.Error("Connection the the Readwise API failed")
 		panic(err)
 	}
 	defer res.Body.Close()
 
 	content, err := io.ReadAll(res.Body)
 	if err != nil {
+		log.Error(err.Error())
 		panic(err)
 	}
-	fmt.Println(string(content))
-	return string(content)
+	var obj JSONPayload
+	err = json.Unmarshal(content, &obj)
+	if err != nil {
+		panic(err)
+	}
+	highlights := obj.Highlights
+	n := rand.Intn(len(highlights))
+	highlight := highlights[n]
+	return highlight
+}
+
+type readwiseHighlight struct {
+	Text          string    `json:"text"`
+	Title         string    `json:"title"`
+	Author        string    `json:"author"`
+	Url           string    `json:"url"`
+	SourceUrl     string    `json:"source_url"`
+	SourceType    string    `json:"source_type"`
+	Category      string    `json:"category"`
+	LocationType  string    `json:"location_type"`
+	Location      int       `json:"location"`
+	Note          string    `json:"note"`
+	HighlightedAt time.Time `json:"highlighted_at"`
+	HightlightUrl string    `json:"highlight_url"`
+	ImageUrl      string    `json:"image_url"`
+	Id            int       `json:"id"`
+	ApiSource     string    `json:"api_source"`
+}
+
+type JSONPayload struct {
+	ReviewId        int                 `json:"review_id"`
+	ReviewUrl       string              `json:"review_url"`
+	ReviewCompleted bool                `json:"review_completed"`
+	Highlights      []readwiseHighlight `json:"highlights"`
 }
 
 var rootCmd = &cobra.Command{
@@ -55,7 +96,9 @@ var rootCmd = &cobra.Command{
 	Short: "readwise-daily print daily highlights to the command line",
 	Run: func(cmd *cobra.Command, args []string) {
 		highlight := getHighlight()
-		fmt.Println(highlight)
+		pterm.DefaultHeader.Println(highlight.Title)
+		pterm.DefaultCenter.Println(highlight.Text)
+		fmt.Println(highlight.Author)
 	},
 }
 
